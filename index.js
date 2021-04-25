6fcbe90c01a68a42f247fcae0ae4ff6b85a4c235
demirbileku <udemirbilek2@gmail.com>
2021-04-25 15:25:49 +0300

    commit tree af0b281e8eb13f3cc91aaf101717d523b70e1cb7

---
+++ index.js
@@ -0,0 +1,34 @@
+function wakeDog(dogName, dogBreed) {
+    const string = `Wake ${dogName} the ${dogBreed}`
+    console.log(string);
+    return string  }
+function leashDog(dogName, dogBreed) {
+    const string = `Leash ${dogName} the ${dogBreed}`
+    console.log(string);
+    return string  }
+function walkToPark(dogName, dogBreed) {
+    const string = `Walk to the park with ${dogName} the ${dogBreed}`
+    console.log(string);
+    return string  }
+function throwFrisbee(dogName, dogBreed) {
+    const string = `Throw the frisbee for ${dogName} the ${dogBreed}`
+    console.log(string);
+    return string  }
+function walkHome(dogName, dogBreed) {
+    const string = `Walk home with ${dogName} the ${dogBreed}`
+    console.log(string);
+    return string   }
+function unleashDog(dogName, dogBreed) {
+    const string = `Unleash ${dogName} the ${dogBreed}`
+    console.log(string);
+    return string  }
+
+let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+    
+function exerciseDog(dogName, dogBreed) {
+    let array = [];
+    for (let i = 0; i < routine.length; i++){
+        array.push(routine[i](dogName, dogBreed));
+        }
+    return array
+}
